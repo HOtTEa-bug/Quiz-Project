ae743da31b1398b4e034ceaac2f93902c95ac824
Shaini Dasgupta <hottea.code@gmail.com>
2021-09-23 22:42:01 +0530

    first commit

---
+++ data.py
@@ -0,0 +1,71 @@
+question_data = [
+    {
+        "category":"Science: Computers",
+        "type":"boolean","difficulty":"medium","question":"The HTML5 standard was published in 2014.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "category":"Science: Computers",
+        "type":"boolean","difficulty":"medium",
+        "question":"The common software-programming acronym &quot;I18N&quot; comes from the term &quot;Interlocalization&quot;.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "category":"Science: Computers",
+        "type":"boolean","difficulty":"easy",
+        "question":"The programming language &quot;Python&quot; is based off a modified version of &quot;JavaScript&quot;.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "category":"Science: Computers",
+        "type":"boolean","difficulty":"easy",
+        "question":"Ada Lovelace is often considered the first computer programmer.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "category":"Science: Computers",
+        "type":"boolean","difficulty":"medium",
+        "question":"MacOS is based on Linux.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "category":"Science: Computers",
+        "type":"boolean","difficulty":"easy",
+        "question":"In most programming languages, the operator ++ is equivalent to the statement &quot;+= 1&quot;.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "category":"Science: Computers",
+        "type":"boolean","difficulty":"hard",
+        "question":"The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "category":"Science: Computers",
+        "type":"boolean","difficulty":"easy",
+        "question":"The Windows 7 operating system has six main editions.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "category":"Science: Computers",
+        "type":"boolean","difficulty":"medium",
+        "question":"The last Windows operating system to be based on the Windows 9x kernel was Windows 98.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "category":"Science: Computers",
+        "type":"boolean","difficulty":"hard",
+        "question":"The T-Mobile Sidekick smartphone is a re-branded version of the Danger Hiptop.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    }
+]
